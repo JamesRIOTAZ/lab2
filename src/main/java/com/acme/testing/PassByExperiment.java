b6360d5c73c6c66b3294957bba1b87a216a80a25
JamesRIOTAZ <jmcdonald.uk@gmail.com>
2019-05-05 22:22:09 +0100

    Lab 7

---
+++ src/main/java/com/acme/testing/PassByExperiment.java
@@ -0,0 +1,50 @@
+package com.acme.testing;
+
+import com.acme.utils.MyDate;
+
+public class PassByExperiment {
+
+	public static void main(String[] args) {
+		MyDate date = new MyDate(1,20,2008);
+		System.out.println("Before passing an object " + date);
+		passObject(date);
+		System.out.println("After passing an object " + date);
+		System.out.println("Before passing a primitive " + date.year);
+		passPrimitive(date.year);
+		System.out.println("After passing a primitive " + date.year);
+		
+		System.out.println("");
+		String x = date.toString();
+		System.out.println("Before passing a String " + x);
+		passString(x);
+		System.out.println("After passing a String " + x);
+		
+		System.out.println("");
+		StringBuilder s = new StringBuilder(date.toString());
+		System.out.println("Before passing a String " + s);
+		passStringBuilder(s);
+		System.out.println("After passing a String " + s);
+
+	}
+	
+	public static void passObject(MyDate d) {
+		d.year=2009;
+	}
+	
+	public static void passPrimitive(int i) {
+		i=2010;
+	}
+	
+	public static void passString(String s) {
+		int yearSlash = s.lastIndexOf('/');
+		s = s.substring(0, yearSlash+1);
+		s += "2012";
+		System.out.println("New date string: "+s);
+	}
+	
+	public static void passStringBuilder(StringBuilder s) {
+		s.replace(s.lastIndexOf("/")+1, s.lastIndexOf("/")+5, "2012");
+		System.out.println("New date string: "+s);
+	}
+
+}
